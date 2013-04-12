964be9214214151cb567bd48e36f633c3cd6694e
Whymarrh Whitby <whymarrh.whitby@gmail.com>
2013-04-12 20:26:55 -0230

    Use strict for Admin JS.

--- kittens/js/admin.js
+++ kittens/js/admin.js
@@ -1,5 +1,7 @@
 !function (window, document, undefined) {
 
+	"use strict";
+
 	var
 	deleteUsersBtn = document.getElementById("delete-users"),
 	deleteDatasetsBtn = document.getElementById("delete-datasets"),
